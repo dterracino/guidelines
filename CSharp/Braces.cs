11da93de77249d40171181a4de0c9e46b5ed6290
Thomas Johansen <thomasjo@gmail.com>
2012-06-26 21:36:26 +0200

    Expand brace examples

--- CSharp/Braces.cs
+++ CSharp/Braces.cs
@@ -11,5 +11,33 @@
                 return true;
             }
         }
+
+        public void SomeMethod()
+        {
+            if (true) {
+                // Do something...
+            }
+            else {
+                // You know the drill
+            }
+
+            using (var reader = new StreamReader()) {
+                var foo = new Foo {
+                    Id = "foo",
+                    Description = "bar"
+                };
+            }
+        }
+
+        private void Bar()
+        {
+            var task = new Task<string>(() => {
+                foreach (var doodad in doodads) {
+                    doodad.Initialize();
+                }
+
+                return "Yay!";
+            });
+        }
     }
 }
